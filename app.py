c1ea08c88637022c5b743174e85ff19fdedaba40
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-06 01:35:18 -0700

    [add] users can now see dops

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask (__name__)
 
 @app.route('/')
-def index():
-    """Return homepage"""
-    return render_template ('home.html', msg='Adoptable dogs!')
+def dogs_index():
+    """Show all dogs"""
+
+    dogs = [
+        {'title': 'Small dogs', 'description': '1-25 lbs.'},
+        {'title': 'Medium dogs', 'description': '26-40 lbs.'},
+        {'title': 'Large dogs', 'description': '41-70 lbs.'},
+        {'title': 'Very large dogs', 'description': '71-above lbs.'}
+    ]
+
+    return render_template('dogs_index.html', dogs=dogs)
 
 if __name__ == '__main__':
     app.run(debug=True)
